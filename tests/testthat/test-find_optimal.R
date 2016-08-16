38ab208728e7de3ae8e4bc89067eecada6c8512a
Mitchell Lyons <mitchell.lyons@gmail.com>
2016-08-16 15:45:49 +1000

    i/o tests

--- tests/testthat/test-find_optimal.R
+++ tests/testthat/test-find_optimal.R
@@ -10,6 +10,15 @@ context("Testing find_optimal() functionality")
 cutreeLevels = 2:10
 
 
+# mixed data
+mixed <- data.frame(a = rpois(n = 42, lambda = 10),
+                     b = rpois(n = 42, lambda = 11),
+                     c = rpois(n = 42, lambda = 12),
+                     d = rpois(n = 42, lambda = 13),
+                     e = rep("char", 42),
+                     f = as.factor(rep("char", 42)),
+                    stringsAsFactors = FALSE)
+
 # dummy counts for tests
 counts <- data.frame(a = rpois(n = 42, lambda = 10),
                      b = rpois(n = 42, lambda = 11),
@@ -21,6 +30,18 @@ counts <- data.frame(a = rpois(n = 42, lambda = 10),
 counts_cutree <- hclust(dist(counts))
 # list of cluster solutions
 counts_list <- lapply(2:10, FUN = function(x, data){cutree(tree = data, k = x)}, data=counts_cutree)
+
+
+## generic input tests
+test_that("find_optimal() stops with wrong data or arguments", {
+  # wrong family spec
+  expect_error(expr = optimus::find_optimal(data = mixed, clustering = counts_cutree, family = "poisson"), message = "Some.*")
+  # not cutree with cutree=T
+  # not list with cutree=F
+  # uneven cluster list lengths
+  # nrow data matches length clustering labels
+  # non numeric vector supplied to cutreeLevels
+})
 
 
 ## count tests
@@ -39,3 +60,6 @@ test_that("find_optimal() returns correct number of rows and columns", {
 test_that("find_optimal() returns correctly named columns", {
   expect_equal(all(names(counts_optimal) %in% c("sum_aic", "nclusters")), TRUE)
 })
+
+# test AIC results are the same if cutree=T and cutree=F generating the same clustering?
+
