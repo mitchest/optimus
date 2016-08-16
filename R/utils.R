5b630388d0c0b356d85016e98d643ec3ecf4c39c
Mitchell Lyons <mitchell.lyons@gmail.com>
2016-08-16 15:46:10 +1000

    init utility funs

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+# hidden utility functions ------------------------------------------------
+
+# Determine if range of vector is FP 0.
+zero_range <- function(x, tol = .Machine$double.eps ^ 0.5) {
+  if (length(x) == 1) return(TRUE)
+  x <- range(x) / mean(x)
+  isTRUE(all.equal(x[1], x[2], tolerance = tol))
+}
