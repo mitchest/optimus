a2376376c904c3c0c425ef2d3e3ddbb55f68f2d5
mitchest <mitchell.lyons@gmail.com>
2016-09-08 20:07:30 +1000

    move clm funs

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,37 @@ zero_range <- function(x, tol = .Machine$double.eps ^ 0.5) {
   x <- range(x) / mean(x)
   isTRUE(all.equal(x[1], x[2], tolerance = tol))
 }
+
+# manyglm analogy for prop odds model
+manyclm <- function(responses, predictor) {
+  if (length(predictor)==1) { # null model
+    aics <- unlist(lapply(X = responses,
+                          FUN = function(x) {stats::AIC(ordinal::clm(formula = x ~ 1))}))
+    variables <- unlist(lapply(X = responses, FUN = names))
+  } else {
+    aics <- unlist(lapply(X = responses,
+                          FUN = function(x, predictor) {stats::AIC(ordinal::clm(formula = x ~ predictor))},
+                          predictor = predictor))
+    variables <- unlist(lapply(X = responses, FUN = names))
+  }
+  names(aics) <- variables
+  aics
+}
+# manyglm analogy for prop odds model - no variable names
+manyclm_naked <- function(responses, predictor) {
+  if (length(predictor)==1) { # null model
+    aics <- unlist(lapply(X = responses,
+                          FUN = function(x) {stats::AIC(ordinal::clm(formula = x ~ 1))}))
+  } else {
+    aics <- unlist(lapply(X = responses,
+                          FUN = function(x, predictor) {stats::AIC(ordinal::clm(formula = x ~ predictor))},
+                          predictor = predictor))
+  }
+  aics
+}
+
+# $AICsum analogy form manyglm for prop odds model
+manyclm_sum <- function(responses, clusters) {
+  sum_clm_aics <- function(x, clusters) {stats::AIC(ordinal::clm(formula = x ~ clusters))}
+  sum(unlist(lapply(X = responses, FUN = sum_clm_aics, clusters = clusters)))
+}
